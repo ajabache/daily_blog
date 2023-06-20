0093aad161eaed0451d4972f7bde3330a1ea2fcf
ajabache <ajtheinventor23@gmail.com>
2023-06-20 16:21:50 +0800

    data is ok

---
+++ static/js/script.js
@@ -0,0 +1,36 @@
+fetch("blogs.json")
+    .then(response => response.json())
+    .then(data => {
+
+        const authorArea = document.getElementById('author');
+        const blogContent = document.getElementById('blogContent');
+        const author = data.blog.author + "'s Blogs";
+        const posts = data.blog.posts;
+
+        authorArea.textContent = author;
+
+        posts.forEach(post => {
+            const { title, date, content } = post;
+
+            const postElement = document.createElement('div');
+            postElement.className = 'post';
+
+            const titleElement = document.createElement('div');
+            titleElement.className = 'text-2xl font-bold text-gray-800';
+            titleElement.textContent = title;
+            postElement.appendChild(titleElement);
+
+            const dateElement = document.createElement('div');
+            dateElement.className = 'date';
+            dateElement.textContent = date;
+            postElement.appendChild(dateElement);
+
+            const contentElement = document.createElement('div');
+            contentElement.className = 'prose';
+            contentElement.innerHTML = content;
+            postElement.appendChild(contentElement);
+
+            blogContent.appendChild(postElement);
+        });
+    })
+    .catch(error => console.error(error));
