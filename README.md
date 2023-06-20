5384a00c41d75a33cbea4c319e53acb6a7239dea
joemar25 <joemar.cardino000@gmail.com>
2023-06-20 23:40:26 +0800

    working routes and profile reader added

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # daily_blog of rjoemar and adrian
+
+todo
+ routing using id
+ if in id blog then change title of the page
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
--- static/js/script.js
+++ static/js/script.js
@@ -1,42 +1,85 @@
-fetch("blogs.json")
-    .then(response => response.json())
-    .then(data => {
+const titleArea = document.getElementById("title");
+const authorArea = document.getElementById("author");
+const blogContent = document.getElementById("blogContent");
 
-        const titleArea = document.getElementById('title');
-        const authorArea = document.getElementById('author');
-        const blogContent = document.getElementById('blogContent');
-        const author = data.blog.author;
-        const title = data.blog.author;
-        const posts = data.blog.posts;
+if (window.location.href.includes("joemar") || window.location.href.includes("adrian")) {
+  fetch("blogs.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const { author, title, posts } = data.blog;
 
-        authorArea.textContent = author + "'s Blogs";
-        titleArea.textContent = title  + "'s Blogs";
+      authorArea.textContent = author;
+      titleArea.textContent = title;
 
-        posts.forEach(post => {
-            const { id, title, date, content } = post;
+      const postTemplate = document.createElement("template");
+      postTemplate.innerHTML = `
+        <div class="post">
+          <div class="text-2xl font-bold text-gray-800"></div>
+          <div class="date"></div>
+          <button class="prose"></button>
+        </div>
+      `;
 
-            const postElement = document.createElement('div');
-            postElement.className = 'post';
+      posts.forEach((post) => {
+        const { id, title, date, content } = post;
 
-            const titleElement = document.createElement('div');
-            titleElement.className = 'text-2xl font-bold text-gray-800';
-            titleElement.textContent = title;
-            postElement.appendChild(titleElement);
+        const postElement = postTemplate.content.cloneNode(true);
+        const titleElement = postElement.querySelector(".text-2xl");
+        const dateElement = postElement.querySelector(".date");
+        const contentElement = postElement.querySelector(".prose");
 
-            const dateElement = document.createElement('div');
-            dateElement.className = 'date';
-            dateElement.textContent = date;
-            postElement.appendChild(dateElement);
+        titleElement.textContent = title;
+        dateElement.textContent = date;
+        contentElement.innerHTML = content;
 
-            const contentElement = document.createElement('button');
-            contentElement.className = 'prose';
-            contentElement.innerHTML = content;
-            contentElement.addEventListener('click', () => {
-                window.location.href = `/pages/${author.toLowerCase()}/post.html?id=${id}`;
-            });
-            postElement.appendChild(contentElement);
-
-            blogContent.appendChild(postElement);
+        contentElement.addEventListener("click", () => {
+          const authorArray = author.split(" ");
+          window.location.href = `/pages/${authorArray[0].toLowerCase()}/post.html?id=${id}`;
         });
+
+        blogContent.appendChild(postElement);
+      });
     })
-    .catch(error => console.error(error));
+    .catch((error) => console.error(error));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (window.location.href.includes("post")) {
+    fetch("blogs.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const { posts, author, title, profileImg } = data.blog;
+        const urlParams = new URLSearchParams(window.location.search);
+        const id = parseInt(urlParams.get("id"));
+        const blog = posts.find((blog) => blog.id === id);
+
+        const blogTitle = document.getElementById("blogTitle");
+        const blogAuthor = document.getElementById("blogAuthor");
+        const blogContent = document.getElementById("blogContent");
+        const dateElement = document.querySelector("time");
+
+        if (blog) {
+          blogTitle.textContent = blog.title;
+          blogAuthor.textContent = author;
+          blogContent.innerHTML = blog.content;
+          titleArea.textContent = `${title} | ${blogTitle.textContent}`;
+
+          dateElement.textContent = blog.date;
+          dateElement.setAttribute("datetime", blog.date);
+          dateElement.setAttribute("title", blog.title);
+        } else {
+          console.error(`Blog post with id ${id} not found`);
+        }
+
+        const profileImgElement = document.getElementById("profileImg");
+        profileImgElement.src = profileImg;
+        profileImgElement.alt = author;
+      })
+      .catch((error) => console.error(error));
+  }
+});
